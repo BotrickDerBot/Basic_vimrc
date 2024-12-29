e9bc84ed88a38a3207c6ba1d4b9a119a73b93a3c
Patrick Klein <learning@patrick-klein.org>
2024-12-29 23:33:00 +0100

    modified

--- config_basic_vimrc.sh
+++ config_basic_vimrc.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Liste der zu prüfenden Ordner
 directories=(
     "$HOME/.vim"
     "$HOME/.vim/autoload"
@@ -6,11 +9,14 @@ directories=(
     "$HOME/.vim/plugged"
 )
 
-for dir in "{$directories[@]}"; do 
-    if [ -d "$dir"]; then
+# Schleife durch die Ordnerliste
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
         echo "Der Ordner $dir existiert bereits."
     else
+        echo "Der Ordner $dir existiert nicht. Er wird angelegt..."
         mkdir -p "$dir" && echo "Ordner $dir wurde erfolgreich erstellt." || echo "Fehler beim Erstellen des Ordners $dir."
     fi
 done
+
 
