d605810bf691542d3f8b663e784a24ee4534d7c3
Patrick Klein <learning@patrick-klein.org>
2025-01-11 22:05:22 +0100

    check if basic_settings.txt exists, if yes write the content into the .vimrc

--- config_basic_vimrc.sh
+++ config_basic_vimrc.sh
@@ -9,6 +9,11 @@ directories=(
     "$HOME/.vim/plugged"
 )
 
+# Pfade zu den Dateien
+BASIC_SETTINGS="basic_settings.txt"
+VIMRC="$HOME/.vimrc"
+
+
 # Schleife durch die Ordnerliste ob diese bereits existieren, falls nicht werden diese erstellt.
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
@@ -26,4 +31,16 @@ else
     echo "Die Datei .vimrc wird erstellt."
     touch .vimrc
 fi
+
+# Überprüfen, ob die basic_settings.txt existiert
+if [ ! -f "$BASIC_SETTINGS" ]; then
+    echo "Fehler: Die Datei $BASIC_SETTINGS existiert nicht."
+    exit 1
+else
+    # Inhalt von basic_settings.txt in die .vimrc schreiben (anhängen)
+    cat "$BASIC_SETTINGS" >> "$VIMRC"
+    echo "Der Inhalt von $BASIC_SETTINGS wurde erfolgreich in $VIMRC geschrieben."
+fi
+
+
 
